3116c1bcd49051415c2de68333c0d472bafc58b1
Shaun Rowe <shaun@poblcreative.com>
2017-10-11 19:13:38 +0100

    Initial Commit

---
+++ RxSwiftExamples/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RxSwiftExamples
+//
+//  Created by Shaun Rowe on 11/10/2017.
+//  Copyright © 2017 Shaun Rowe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
